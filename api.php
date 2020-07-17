d1b82781ac79d4c87acdfd3b464a611ac7334923
Oliver <draper87@gmail.com>
2020-07-17 15:58:42 +0200

    6

--- api.php
+++ api.php
@@ -1,10 +1,15 @@
 <?php
+
+  // API che restituisce come risultato il database con gli album musicali
   include 'database.php';
 
+  // formatto il database ottenuto in formato json
   $database_json = json_encode($database);
 
+  // comunico che il formato utilizzato e json
   header('Content-Type: application/json');
 
+  // stampo il risultato
   echo $database_json;
 
  ?>
--- apifiltro.php
+++ apifiltro.php
@@ -1,14 +1,18 @@
 <?php
+  // API che restituisce come risultato il database con gli album musicali, in questo caso il database viene filtrato in base
+  // alla select fatta dall utente direttamente dalla pagina web.
+
+  // includo il database
   include 'database.php';
 
+  // mi prendo il valore dalla chiamata Ajax che a sua volta lo prende dal .val() della select
   $artista = $_GET["artista"];
 
-
+  // mi creo un array vuoto che andro a popolare con i soli dati filtrati del mio database originario
   $array_artista = [];
 
-  // var_dump($artista);
-
-
+  // faccio un ciclo che va a popolare l array appena creato, faccio push solamente nel caso in cui l autore corrisponda
+  // a quello che ha scelto l utente
   foreach ($database as $singolo_cd) {
     if (($singolo_cd['author'] == $artista)) {
       $array_artista[] = $singolo_cd;
@@ -16,12 +20,13 @@
 
   }
 
-  // var_dump($array_artista);
-
+  // formatto il database ottenuto in formato json
   $database_json = json_encode($array_artista);
 
+  // comunico che il formato utilizzato e json
   header('Content-Type: application/json');
 
+  // stampo il risultato
   echo $database_json;
 
  ?>
